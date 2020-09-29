192a1b5c6b2b80149a998af829b27189b2f1212b
Chet Murthy <chetsky@gmail.com>
2020-09-28 21:53:45 -0700

    starting to work
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   pa_simplify/pa_simplify.ml
    #
    # Changes not staged for commit:
    #	modified:   tests/include_ml
    #
    # Untracked files:
    #	tests/TEST-PP
    #	tests/test1.ml
    #

--- pa_simplify/pa_simplify.ml
+++ pa_simplify/pa_simplify.ml
@@ -5,6 +5,7 @@
 #load "q_MLast.cmo";
 #load "pa_extfun.cmo";
 
+open MLast ;
 open Pa_ppx_base ;
 open Pa_ppx_utils ;
 open Pa_passthru ;
@@ -36,7 +37,8 @@ open Ppxutil ;
 module FVS = struct
   type t = option (list string) ;
 
-  value union s1 s2 = match (s1, s2) with [
+  value empty = fun [ Some [] -> True | _ -> False ] ;
+  value union (s1 : t) s2 = match (s1, s2) with [
     (Some s1, Some s2) -> Some (Std.union s1 s2)
   | _ -> None
   ]
@@ -57,8 +59,8 @@ module FVS = struct
   ]
   ;
 
-  value intersect s1 s2 = match (s1,s2) with [
-    (Some s1, s2) -> Some (Std.intersect s1 s1)
+  value intersect (s1 : t) s2 = match (s1,s2) with [
+    (Some s1, Some s2) -> Some (Std.intersect s1 s2)
   | _ -> None
   ]
   ;
@@ -67,16 +69,17 @@ end
 
 module Rho = struct
   type t = list (string * (MLast.expr * FVS.t)) ;
+  value dom l = List.map fst l ;
   value ofList l = l ;
   value mem id l = List.mem_assoc id l ;
   value map id l = List.assoc id l ;
-  value except rho fvs = match fvs with [
+  value except (rho : t) (fvs : FVS.t) = match fvs with [
     None -> []
   | Some l -> Std.filter (fun (id, _) -> not (List.mem id l)) rho
   ]
   ;
   value captures rho fvs =
-    List.exists (fun (_, (_, fvs')) -> Some [] <> FVS.intersect fvs fvs') rho ;
+    List.exists (fun (_, (_, fvs')) -> not (FVS.empty (FVS.intersect fvs fvs'))) rho ;
 end
 ;
 
@@ -86,6 +89,15 @@ value patt_fvs p =
   | <:patt< ( $list:l$ ) >> -> List.fold_left (fun acc p -> Std.union acc (prec p)) [] l
   | <:patt< { $list:lpl$ } >> ->
     List.fold_left (fun acc -> fun (_, p) -> Std.union acc (prec p)) [] lpl
+  | <:patt< $p1$ $p2$ >> ->
+    Std.union (prec p1) (prec p2)
+  | <:patt< $longid:_$ >> -> []
+  | <:patt< ( $p$ : $_$ ) >> -> prec p
+
+  | <:patt< _ >> -> []
+
+  | p -> Ploc.raise (loc_of_patt p)
+      (Failure Fmt.(str "patt_fvs: unimplemented %a" Pp_MLast.pp_patt p))
   ] in
   prec p
 ;
@@ -105,13 +117,31 @@ value pure e =
   prec e
   ;
 
-value subst rho e =
+value conservative_freevars e =
+  let rec crec = fun [
+    <:expr< $uid:_$ >> -> FVS.ofList []
+  | <:expr< $lid:lid$ >> -> FVS.ofList [lid]
+  | <:expr< $e1$ . $e2$ >> -> FVS.union (crec e1) (crec e2)
+  | e -> Ploc.raise (loc_of_expr e)
+      (Failure Fmt.(str "conservative_freevars: unimplemented %a" Pp_MLast.pp_expr e))
+  ] in
+  crec e
+;
+
+value subst (rho : Rho.t) e =
   let rec srec rho e =
     if rho = [] then e else
       match e with [
     <:expr:< $lid:lid$ >> as e ->
     if List.mem_assoc lid rho then fst (List.assoc lid rho)
     else e
+
+  | <:expr:< $_$ . $_$ >> as z ->
+    if not (FVS.empty (FVS.intersect (FVS.ofList (Rho.dom rho)) (conservative_freevars z))) then
+      failwith "caught"
+    else z
+
+  | <:expr:< $e1$ $e2$ >> -> <:expr< $srec rho e1$ $srec rho e2$ >>
 
   | <:expr:< ( $list:l$ ) >> ->
     <:expr< ( $list:List.map (srec rho) l$ ) >>
@@ -129,21 +159,22 @@ value subst rho e =
         (p, vala_map (Option.map (srec stripped_rho)) whene, srec stripped_rho e) in
     <:expr< (fun [ $list:List.map br1 branches$ ]) >>
 
+  | e -> Ploc.raise (loc_of_expr e)
+      (Failure Fmt.(str "subst: unimplemented %a" Pp_MLast.pp_expr e))
+
   ] in
   try Some (srec rho e)
   with [ Failure "caught" -> None ]
 ;
 
+value beta_reduce (l : expr) (r : expr) = () ;
+value eta_reduce (l : expr) (r : expr) = () ;
+
 value simplify e =
   let rec simprec = fun [
     <:expr< $lid:lid$ >> as z -> (z, FVS.ofList [lid])
 
-  | <:expr:< $e1$ $e2$ >> ->
-    let (e1, fv1) = simprec e1 in
-    let (e2, fv2) = simprec e1 in
-    (<:expr< $e1$ $e2$ >>, FVS.union fv1 fv2)
-
-  | <:expr:< (fun $lid:x$ -> $_M$) $_N$ >> ->
+  | <:expr:< (fun $lid:x$ -> $_M$) $_N$ >> as z ->
     let (_M, fvM) = simprec _M in
     let (_N, fvN) = simprec _N in
     if not (pure _N) then
@@ -154,10 +185,101 @@ value simplify e =
         None -> 
         (<:expr< (fun $lid:x$ -> $_M$) $_N$ >>,
          FVS.union fvN (FVS.except fvM [x]))
+      | Some _M' -> do {
+          beta_reduce <:expr< (fun $lid:x$ -> $_M$) $_N$ >> _M' ;
+          (_M', FVS.union (FVS.except fvM [x]) fvN)
+        }
+      ]
+    else 
+      (_M, fvM)
+
+  | <:expr:< (fun ( $lid:x$ : $t$ ) -> $_M$) $_N$ >> ->
+    let (_M, fvM) = simprec _M in
+    let (_N, fvN) = simprec _N in
+    if not (pure _N) then
+      (<:expr< (fun ( $lid:x$ : $t$ ) -> $_M$) $_N$ >>,
+       FVS.union fvN (FVS.except fvM [x]))
+    else if FVS.mem x fvM then
+      match subst [(x, (_N,fvN))] _M with [
+        None -> 
+        (<:expr< (fun ( $lid:x$ : $t$ ) -> $_M$) $_N$ >>,
+         FVS.union fvN (FVS.except fvM [x]))
       | Some _M' -> (_M', FVS.union (FVS.except fvM [x]) fvN)
       ]
     else 
       (_M, fvM)
+
+  | <:expr< $e$ && True >> -> simprec e
+  | <:expr< True && $e$ >> -> simprec e
+
+  | <:expr< $e$ || False >> -> simprec e
+  | <:expr< False || $e$ >> -> simprec e
+
+  | <:expr< $e$ + 0 >> -> simprec e
+  | <:expr< 0 + $e$ >> -> simprec e
+
+  | <:expr:< $e1$ $e2$ >> ->
+    let (e1, fv1) = simprec e1 in
+    let (e2, fv2) = simprec e2 in
+    (<:expr< $e1$ $e2$ >>, FVS.union fv1 fv2)
+
+  | <:expr:< (fun $lid:x$ -> $_M$ $lid:x'$ ) >> as z when x = x' ->
+      let (_M, fvM) = simprec _M in
+      if not (FVS.mem x fvM) then do {
+        eta_reduce <:expr:< (fun $lid:x$ -> $_M$ $lid:x'$ ) >> _M ;
+        (_M, fvM)
+      }
+      else
+        (<:expr:< (fun $lid:x$ -> $_M$ $lid:x$ ) >>,
+         FVS.except fvM [x])
+
+  | <:expr:< (fun ( $lid:x$ : $t$ ) -> $_M$ $lid:x'$ ) >> as z when x = x' ->
+      let (_M, fvM) = simprec _M in
+      if not (FVS.mem x fvM) then do {
+        eta_reduce <:expr:< (fun ( $lid:x$ : $t$ ) -> $_M$ $lid:x'$ ) >> _M ;
+        (_M, fvM)
+      }
+      else
+        (<:expr:< (fun ( $lid:x$ : $t$ ) -> $_M$ $lid:x$ ) >>,
+         FVS.except fvM [x])
+
+  | <:expr:< (fun [ $list:branches$ ]) >> ->
+    let br1 (p, whene, e) =
+      let (whene,whenefvs) = match uv whene with [
+        None -> (None, FVS.ofList[])
+      | Some e -> 
+        let (e, fvs) = simprec e in (Some e, fvs)
+      ] in
+      let (e,efvs) = simprec e in
+      let fvs = FVS.except (FVS.union whenefvs efvs) (patt_fvs p) in
+      ((p, <:vala< whene >>, e), fvs) in
+    let branches_fvs = List.map br1 branches in
+    let fvs = List.fold_left (fun acc (_, fvs) -> FVS.union acc fvs) (FVS.ofList[])  branches_fvs in
+    let branches = List.map fst branches_fvs in
+    (<:expr:< (fun [ $list:branches$ ]) >>, fvs)
+
+  | <:expr:< (match $e$ with [ $list:branches$ ]) >> ->
+    let (e,efvs) = simprec e in
+    let br1 (p, whene, e) =
+      let (whene,whenefvs) = match uv whene with [
+        None -> (None, FVS.ofList[])
+      | Some e -> 
+        let (e, fvs) = simprec e in (Some e, fvs)
+      ] in
+      let (e,efvs) = simprec e in
+      let fvs = FVS.except (FVS.union whenefvs efvs) (patt_fvs p) in
+      ((p, <:vala< whene >>, e), fvs) in
+    let branches_fvs = List.map br1 branches in
+    let fvs = List.fold_left (fun acc (_, fvs) -> FVS.union acc fvs) efvs  branches_fvs in
+    let branches = List.map fst branches_fvs in
+    (<:expr:< (match $e$ with [ $list:branches$ ]) >>, fvs)
+
+  | <:expr:< ( $list:l$ ) >> ->
+    let lfvs = List.map simprec l in
+    let fvs = List.fold_left (fun acc (_, fvs) -> FVS.union acc fvs) (FVS.ofList[])  lfvs in
+    (<:expr:< ( $list:List.map fst lfvs$ ) >>, fvs)
+
+  | e -> (e, None)
 
   ] in
   fst (simprec e)
